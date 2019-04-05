3e998c1d6a8f29e53469d9a6b86cbf9b428bb900
Zach Weintraub <zachweintraub@gmail.com>
2019-04-05 10:53:58 -0700

    add images and image swapping functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function beepBoop(input) {
   var num = parseInt(input);
   var outputArr = [];
-  if(isNaN(num) || num < 1) {
+  if(isNaN(num) || num < 0) {
     outputArr.push('You call that a number, Dave?')
   } else {
-    for(var i = 1; i < num+1; i++) {
+    for(var i = 0; i < num+1; i++) {
       if(i.toString().split('').includes('3')) {
         outputArr.push('I\'m sorry, Dave. I\'m afraid I can\'t do that.');
       } else if (i.toString().split('').includes('2')) {
@@ -17,12 +17,27 @@ function beepBoop(input) {
   return outputArr;
 }
 
+function picSwap(currentPic) {
+  var picsArr = ['pic1', 'pic2', 'pic3', 'pic4', 'pic5'];
+  var currentIndex = picsArr.indexOf(currentPic);
+  if (currentIndex < picsArr.length-1) {
+    return picsArr[currentIndex + 1];
+  } else return picsArr[0];
+}
+
 $(function(){
   $('#userInput').keyup(function(){
+    var currentPic = $('.show').attr('id');
+    console.log(currentPic);
+    $('img').removeClass();
+    $('#' + picSwap(currentPic)).addClass('show');
     $('#outputList').empty($('#outputList'));
     beepBoop($('#userInput').val()).forEach(function(output){
       $('#outputList').append('<li>' + output + '</li>');
-    })
-  })
+    });
+    if($('#userInput').val() === '') {
+      $('#outputList').empty($('#outputList'));
+    }
+  });
 
 });
